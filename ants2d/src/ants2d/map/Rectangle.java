a666a1dd251e4f1e882940dbfee45f1d50f60247
sergey <mist.patona@gmail.com>
2015-09-27 19:29:43 +0300

    splitting

--- ants2d/src/ants2d/map/Rectangle.java
+++ ants2d/src/ants2d/map/Rectangle.java
@@ -1,5 +1,8 @@
 package ants2d.map;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rectangle implements AB {
 	
 	private Point p0,p1;
@@ -12,6 +15,32 @@ public class Rectangle implements AB {
 	public Rectangle(final AB area) {
 		p0 = area.getCorner(Quadrants.NW);
 		p1 = area.getCorner(Quadrants.SE);
+	}
+	
+	public List<Rectangle> split4() {
+		List<Rectangle> ans = new ArrayList<Rectangle>();
+		//Offset diag = dimentions();//p1.sub(p0);
+		//Offset d2 = new Offset(diag.scaleBy(0.5));
+		Point p = new Point(p0.sum(dimentions().scaleBy(0.5))); // center point
+		ans.add(new Rectangle(p0,p));
+		ans.add(new Rectangle(new Point(p.getX(),p0.getY()),new Point(p1.getX(),p.getY())));
+		ans.add(new Rectangle(new Point(p0.getX(),p.getY()),new Point(p.getX(),p1.getY())));
+		ans.add(new Rectangle(p,p1));
+		return ans;
+	}
+	
+	protected List<Rectangle> split2(Offset d) {
+		List<Rectangle> ans = new ArrayList<Rectangle>(2);
+		ans.add(new Rectangle(p0,new Point(p1.sub(d))));
+		ans.add(new Rectangle(new Point(p0.sum(d)),p1));
+		return ans;
+	}
+	
+	public List<Rectangle> split2h() {  // like in Need for Speed double-player
+		return split2(new Offset(0,dimentions().scaleBy(0.5).getY()));
+	}
+	public List<Rectangle> split2v() { // like in Norton Commander
+		return split2(new Offset(dimentions().scaleBy(0.5).getX(),0));
 	}
 
 	@Override
--- ants2d/src/ants2d/map/RectangleTest.java
+++ ants2d/src/ants2d/map/RectangleTest.java
@@ -3,6 +3,11 @@ package ants2d.map;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class RectangleTest {
 	@Test
 	public void insideTest() {
@@ -22,6 +27,56 @@ public class RectangleTest {
 		assertFalse(r.contains(p_out_3));
 		assertFalse(r.contains(p_out_4));
 		
+	}
+	@Test
+	public void split4CountTest() {
+		Point p0 = new Point(0,0);
+		Point p1 = new Point(8,8);
+		Rectangle r = new Rectangle(p0,p1);
+		List<Rectangle> s = r.split4();
+		assertEquals("split into 4 parts",4,s.size());	
+	}
+	@Test
+	public void split4TestFirsts() {
+		Point p0 = new Point(0,0);
+		Point p1 = new Point(8,8);
+		Rectangle r = new Rectangle(p0,p1);
+		List<Rectangle> s = r.split4();
+		Point pnw = new Point(1,1);
+		Point pne = new Point(7,1);
+		Point psw = new Point(1,7);
+		Point pse = new Point(7,7);
+		Point pout = new Point(-1,-1);
+		assertEquals(firstMatchIndex(s.iterator(),pnw),(Integer)0);
+		assertEquals(firstMatchIndex(s.iterator(),pne),(Integer)1);
+		assertEquals(firstMatchIndex(s.iterator(),psw),(Integer)2);
+		assertEquals(firstMatchIndex(s.iterator(),pse),(Integer)3);
+		assertEquals(firstMatchIndex(s.iterator(),pout),(Integer)(-1));
+		/*   */
+		List<Point> ps = Arrays.asList(pnw,pne,psw,pse);
+		for (Point p : ps) {
+			List<Integer> is = matchesIndexes(s.iterator(),p);
+			assertEquals(is.size(),1);
+		}
+	}
+	
+	private Integer firstMatchIndex(Iterator<Rectangle> ii, Point p)  {
+		for (int c = 0;ii.hasNext();c++) {
+			if (ii.next().contains(p)) {
+				return c;
+			}
+		}
+		return -1;
+	}
+	private List<Integer> matchesIndexes(Iterator<Rectangle> ii, Point p)  {
+		List<Integer> ans = new ArrayList<Integer>(4);
+		Integer c = 0;
+		for (;ii.hasNext();c++) {
+			if (ii.next().contains(p)) {
+				ans.add(c);
+			}
+		}
+		return ans;
 	}
 
 }
